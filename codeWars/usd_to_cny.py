1607b28a316a0a4fca42e995866949afffef3908
wezhall <wesleyericksonhall@gmail.com>
2022-03-22 20:11:52 +0000

    Added converted expression

--- codeWars/usd_to_cny.py
+++ codeWars/usd_to_cny.py
@@ -1,1 +1,5 @@
 # Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+def usdcny(usd):
+	cny = usd * 6.75
+	
